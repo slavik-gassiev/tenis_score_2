80e836f1a3089543f76b59f0ec2db7709e470ac2
slavik-gassiev <slavikgassiev1872002@gmail.com>
2024-08-26 13:46:47 +0300

    Added hibernate

--- src/main/java/com/slava/dao/PlayerDAO.java
+++ src/main/java/com/slava/dao/PlayerDAO.java
@@ -4,8 +4,10 @@ import com.slava.dao.interfaces.IPlayerDAO;
 import com.slava.entity.Player;
 
 public class PlayerDAO implements IPlayerDAO<Player, Long> {
+
     @Override
     public Long createPlayer(Player player) {
+
         return null;
     }
 }
---
+++ src/main/java/com/slava/util/fjeijf.java
@@ -0,0 +1,4 @@
+package com.slava.util;
+
+public class fjeijf {
+}
